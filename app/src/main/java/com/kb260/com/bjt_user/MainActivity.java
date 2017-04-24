1651ba81aa7941fd8fc49f260adbe45c30f3e506
kb260 <kb260261@gmail.com>
2017-04-24 22:31:43 +0800

    创建项目 4.24

---
+++ app/src/main/java/com/kb260/com/bjt_user/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kb260.com.bjt_user;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
